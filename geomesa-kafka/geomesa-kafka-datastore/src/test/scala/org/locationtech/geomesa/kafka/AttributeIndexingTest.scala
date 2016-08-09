748b5cc88a172a36ab16ca20b9096083bc3594e9
Jim Hughes <jnh5y@ccri.com>
2016-08-09 16:31:06 -0400

    More updates.

--- geomesa-kafka/geomesa-kafka-datastore/src/test/scala/org/locationtech/geomesa/kafka/AttributeIndexingTest.scala
+++ geomesa-kafka/geomesa-kafka-datastore/src/test/scala/org/locationtech/geomesa/kafka/AttributeIndexingTest.scala
@@ -10,9 +10,11 @@ package org.locationtech.geomesa.kafka
 
 import com.google.common.base.Ticker
 import com.vividsolutions.jts.geom.Point
+import org.geotools.factory.CommonFactoryFinder
 import org.geotools.feature.simple.SimpleFeatureBuilder
-import org.joda.time.{DateTime, DateTimeZone}
-import org.locationtech.geomesa.kafka.LiveFeatureCache
+import org.geotools.filter.text.ecql.ECQL
+import org.joda.time.{DateTime, DateTimeZone, Instant}
+import org.locationtech.geomesa.kafka.{CreateOrUpdate, LiveFeatureCache}
 import org.locationtech.geomesa.utils.geotools.SimpleFeatureTypes
 import org.locationtech.geomesa.utils.text.WKTUtils
 import org.opengis.feature.simple.SimpleFeature
@@ -20,6 +22,10 @@ import org.opengis.feature.simple.SimpleFeature
 import scala.util.Random
 
 class AttributeIndexingTest {
+  implicit def sfToCreate(feature: SimpleFeature): CreateOrUpdate = CreateOrUpdate(Instant.now, feature)
+
+  val ff = CommonFactoryFinder.getFilterFactory2
+
   val spec = "Who:String:index=full,What:Int,When:Date,*Where:Point:srid=4326,Why:String"
   val MIN_DATE = new DateTime(2014, 1, 1, 0, 0, 0, DateTimeZone.forID("UTC"))
   val seconds_per_year = 365L * 24L * 60L * 60L
@@ -31,12 +37,14 @@ class AttributeIndexingTest {
   val builder = new SimpleFeatureBuilder(sft)
 
   val names = Array("Addams", "Bierce", "Clemens", "Damon", "Evan", "Fred", "Goliath", "Harry")
-  def getName: String = names(Random.nextInt(4))
+  def getName: String = names(Random.nextInt(names.length))
 
-  def getPoint: Point = {val minx = -180
+  def getPoint: Point = {
+    val minx = -180
     val miny = -90
     val dx = 360
     val dy = 180
+
     val x = minx + Random.nextDouble * dx
     val y = miny + Random.nextDouble * dy
     WKTUtils.read(s"POINT($x $y)").asInstanceOf[Point]
@@ -45,7 +53,6 @@ class AttributeIndexingTest {
   def buildFeature(i: Int): SimpleFeature = {
     builder.set("Who", getName)
     builder.set("What", Random.nextInt(10))
-
     builder.set("When", randDate)
     builder.set("Where", getPoint)
     if (Random.nextBoolean()) { builder.set("Why", string) }
@@ -62,7 +69,22 @@ class AttributeIndexingTest {
   implicit val ticker = Ticker.systemTicker()
   val lfc = new LiveFeatureCache(sft, None)
 
+  val ab = ECQL.toFilter("Who IN('Addams', 'Bierce')")
+  val cd = ECQL.toFilter("Who IN('Clemens', 'Damon')")
 
+  val w14 = ECQL.toFilter("What = 1 OR What = 2 OR What = 3 or What = 4")
 
+  val where = ECQL.toFilter("BBOX(Where, 0, 0, 180, 90)")
 
+  val justified = ECQL.toFilter("Why is not null")
+
+  val justifiedAB = ff.and(ff.and(ab, w14), justified)
+  val justifiedCD = ff.and(ff.and(cd, w14), justified)
+
+  val just = ff.or(justifiedAB, justifiedCD)
+
+  val justBBOX = ff.and(just, where)
+
+  val feats = (0 until 100000).map(buildFeature)
+  feats.foreach{lfc.createOrUpdateFeature(_)}
 }
