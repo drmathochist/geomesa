0e5731b4079d2b2f2c8478d7e8852bb9533a8649
Matthew Zimmerman <matt.zimmerman@ccri.com>
2016-08-22 15:23:17 -0400

    Implement >,>=,<,<= in the ugliest way possible.

--- geomesa-memory/src/main/scala/org/locationtech/geomesa/memory/cqengine/utils/CQEngineQueryVisitor.scala
+++ geomesa-memory/src/main/scala/org/locationtech/geomesa/memory/cqengine/utils/CQEngineQueryVisitor.scala
@@ -18,6 +18,7 @@ import org.opengis.feature.simple.{SimpleFeature, SimpleFeatureType}
 import org.opengis.filter._
 import org.opengis.filter.expression.{Literal, PropertyName}
 import org.opengis.filter.spatial._
+import org.opengis.filter.temporal._
 
 import scala.collection.JavaConversions._
 import scala.language._
@@ -72,6 +73,114 @@ class CQEngineQueryVisitor(sft: SimpleFeatureType) extends AbstractFilterVisitor
     new com.googlecode.cqengine.query.simple.Equal(attribute, value)
   }
 
+  override def visit(filter: PropertyIsGreaterThan, data: scala.Any): AnyRef = {
+    val prop = getAttributeProperty(filter).get
+    val attributeName = prop.name
+    sft.getDescriptor(attributeName).getType.getBinding match {
+      case c if classOf[java.lang.Integer].isAssignableFrom(c) => {
+        val attr = lookup.lookupComparable[java.lang.Integer](attributeName)
+        val value = prop.literal.evaluate(null, classOf[java.lang.Integer])
+        new com.googlecode.cqengine.query.simple.GreaterThan[SimpleFeature, java.lang.Integer](attr, value, false)
+      }
+      case c if classOf[java.lang.Long].isAssignableFrom(c) => {
+        val attr = lookup.lookupComparable[java.lang.Long](attributeName)
+        val value = prop.literal.evaluate(null, classOf[java.lang.Long])
+        new com.googlecode.cqengine.query.simple.GreaterThan[SimpleFeature, java.lang.Long](attr, value, false)
+      }
+      case c if classOf[java.lang.Float].isAssignableFrom(c) => {
+        val attr = lookup.lookupComparable[java.lang.Float](attributeName)
+        val value = prop.literal.evaluate(null, classOf[java.lang.Float])
+        new com.googlecode.cqengine.query.simple.GreaterThan[SimpleFeature, java.lang.Float](attr, value, false)
+      }
+      case c if classOf[java.lang.Double].isAssignableFrom(c) => {
+        val attr = lookup.lookupComparable[java.lang.Double](attributeName)
+        val value = prop.literal.evaluate(null, classOf[java.lang.Double])
+        new com.googlecode.cqengine.query.simple.GreaterThan[SimpleFeature, java.lang.Double](attr, value, false)
+      }
+    }
+  }
+
+  override def visit(filter: PropertyIsGreaterThanOrEqualTo, data: scala.Any): AnyRef = {
+    val prop = getAttributeProperty(filter).get
+    val attributeName = prop.name
+    sft.getDescriptor(attributeName).getType.getBinding match {
+      case c if classOf[java.lang.Integer].isAssignableFrom(c) => {
+        val attr = lookup.lookupComparable[java.lang.Integer](attributeName)
+        val value = prop.literal.evaluate(null, classOf[java.lang.Integer])
+        new com.googlecode.cqengine.query.simple.GreaterThan[SimpleFeature, java.lang.Integer](attr, value, true)
+      }
+      case c if classOf[java.lang.Long].isAssignableFrom(c) => {
+        val attr = lookup.lookupComparable[java.lang.Long](attributeName)
+        val value = prop.literal.evaluate(null, classOf[java.lang.Long])
+        new com.googlecode.cqengine.query.simple.GreaterThan[SimpleFeature, java.lang.Long](attr, value, true)
+      }
+      case c if classOf[java.lang.Float].isAssignableFrom(c) => {
+        val attr = lookup.lookupComparable[java.lang.Float](attributeName)
+        val value = prop.literal.evaluate(null, classOf[java.lang.Float])
+        new com.googlecode.cqengine.query.simple.GreaterThan[SimpleFeature, java.lang.Float](attr, value, true)
+      }
+      case c if classOf[java.lang.Double].isAssignableFrom(c) => {
+        val attr = lookup.lookupComparable[java.lang.Double](attributeName)
+        val value = prop.literal.evaluate(null, classOf[java.lang.Double])
+        new com.googlecode.cqengine.query.simple.GreaterThan[SimpleFeature, java.lang.Double](attr, value, true)
+      }
+    }
+  }
+
+  override def visit(filter: PropertyIsLessThan, data: scala.Any): AnyRef = {
+    val prop = getAttributeProperty(filter).get
+    val attributeName = prop.name
+    sft.getDescriptor(attributeName).getType.getBinding match {
+      case c if classOf[java.lang.Integer].isAssignableFrom(c) => {
+        val attr = lookup.lookupComparable[java.lang.Integer](attributeName)
+        val value = prop.literal.evaluate(null, classOf[java.lang.Integer])
+        new com.googlecode.cqengine.query.simple.LessThan[SimpleFeature, java.lang.Integer](attr, value, false)
+      }
+      case c if classOf[java.lang.Long].isAssignableFrom(c) => {
+        val attr = lookup.lookupComparable[java.lang.Long](attributeName)
+        val value = prop.literal.evaluate(null, classOf[java.lang.Long])
+        new com.googlecode.cqengine.query.simple.LessThan[SimpleFeature, java.lang.Long](attr, value, false)
+      }
+      case c if classOf[java.lang.Float].isAssignableFrom(c) => {
+        val attr = lookup.lookupComparable[java.lang.Float](attributeName)
+        val value = prop.literal.evaluate(null, classOf[java.lang.Float])
+        new com.googlecode.cqengine.query.simple.LessThan[SimpleFeature, java.lang.Float](attr, value, false)
+      }
+      case c if classOf[java.lang.Double].isAssignableFrom(c) => {
+        val attr = lookup.lookupComparable[java.lang.Double](attributeName)
+        val value = prop.literal.evaluate(null, classOf[java.lang.Double])
+        new com.googlecode.cqengine.query.simple.LessThan[SimpleFeature, java.lang.Double](attr, value, false)
+      }
+    }
+  }
+
+  override def visit(filter: PropertyIsLessThanOrEqualTo, data: scala.Any): AnyRef = {
+    val prop = getAttributeProperty(filter).get
+    val attributeName = prop.name
+    sft.getDescriptor(attributeName).getType.getBinding match {
+      case c if classOf[java.lang.Integer].isAssignableFrom(c) => {
+        val attr = lookup.lookupComparable[java.lang.Integer](attributeName)
+        val value = prop.literal.evaluate(null, classOf[java.lang.Integer])
+        new com.googlecode.cqengine.query.simple.LessThan[SimpleFeature, java.lang.Integer](attr, value, true)
+      }
+      case c if classOf[java.lang.Long].isAssignableFrom(c) => {
+        val attr = lookup.lookupComparable[java.lang.Long](attributeName)
+        val value = prop.literal.evaluate(null, classOf[java.lang.Long])
+        new com.googlecode.cqengine.query.simple.LessThan[SimpleFeature, java.lang.Long](attr, value, true)
+      }
+      case c if classOf[java.lang.Float].isAssignableFrom(c) => {
+        val attr = lookup.lookupComparable[java.lang.Float](attributeName)
+        val value = prop.literal.evaluate(null, classOf[java.lang.Float])
+        new com.googlecode.cqengine.query.simple.LessThan[SimpleFeature, java.lang.Float](attr, value, true)
+      }
+      case c if classOf[java.lang.Double].isAssignableFrom(c) => {
+        val attr = lookup.lookupComparable[java.lang.Double](attributeName)
+        val value = prop.literal.evaluate(null, classOf[java.lang.Double])
+        new com.googlecode.cqengine.query.simple.LessThan[SimpleFeature, java.lang.Double](attr, value, true)
+      }
+    }
+  }
+
   def extractAttributeAndValue(filter: Filter): (Attribute[SimpleFeature, Any], Any) = {
     val prop = getAttributeProperty(filter).get
     val attributeName = prop.name
@@ -81,20 +190,17 @@ class CQEngineQueryVisitor(sft: SimpleFeatureType) extends AbstractFilterVisitor
   }
 
   // JNH: TODO: revisit if this this needed.
-  override def toString = s"CQEngineQueryVisit()"
-
-  override def visit(filter: PropertyIsGreaterThan, data: scala.Any): AnyRef = {
-    val (attribute: Attribute[SimpleFeature, Integer], value: Integer) = extractIntegerAttributeAndValue(filter)
-    new com.googlecode.cqengine.query.simple.GreaterThan(attribute, value, false)
-  }
+  override def toString = "CQEngineQueryVisit()"
 
   // Dealing with the comparable business is tough:(
-  def extractIntegerAttributeAndValue(filter: Filter): (Attribute[SimpleFeature, Integer], Integer) = {
+  /*
+  def extractComparableAttributeAndValue[T](filter: Filter): (Attribute[SimpleFeature, T], T) = {
     val prop = getAttributeProperty(filter).get
     val attributeName = prop.name
 
-    val attribute = lookup.lookupComparable[Integer](attributeName)
+    val attribute = lookup.lookupComparable[T](attributeName)
     val value = prop.literal.evaluate(null, attribute.getAttributeType)
     (attribute, value)
   }
+  */
 }
